3718345642f38aa872900c217d235219684fc975
Aditya choudhry <131998591+Aditya-Choudhry@users.noreply.github.com>
2024-07-29 06:24:33 +0530

    Delete sphereoutsourcing--Qr-Code-main directory

--- sphereoutsourcing--Qr-Code-main/qRcode/server/src/config/config.js
+++
@@ -1,6 +0,0 @@
-
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const SESSION_DURATION = process.env.SESSION_DURATION || '1h';
--- sphereoutsourcing--Qr-Code-main/qRcode/server/src/routes/contact-router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import contactRateLimiter from '../middlewares/rateLimiter.js';
-import contactForm from '../controllers/contact-controller.js';
-
-const router = express.Router();
-
-router.post('/contact', contactRateLimiter, contactForm);
-
-export default router;
